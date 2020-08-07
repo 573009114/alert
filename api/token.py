6f0e05a07e6a858df1046f7edc6d1f5970445e39
风华 <573009114@qq.com>
2020-08-07 14:37:50 +0800

    Update token.py

--- api/token.py
+++ api/token.py
@@ -1,7 +1,7 @@
 import requests
 
-#url = "http://10.69.41.156:9123/openapi/api-auth/"
-url = "http://10.69.41.156:9123/openapi/api-token-auth/"
+
+url = "http://10.18.41.156:9123/openapi/api-token-auth/"
 payload = {
     'username': 'admin',
     'password': 'admin@888'
